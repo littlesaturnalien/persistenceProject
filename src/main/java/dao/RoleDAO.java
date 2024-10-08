9ddfe0e51bf3ef486c70dcfd02332b52e8a4e43f
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2024-10-08 15:45:59 -0600

    Interfaz iRole con su clase RoleDAO finalizada

--- src/main/java/dao/RoleDAO.java
+++ src/main/java/dao/RoleDAO.java
@@ -1,4 +1,41 @@
 package dao;
 
-public class RoleDAO {
+import interfaces.iRole;
+import jakarta.persistence.EntityManager;
+import models.Role;
+import models.User;
+
+public class RoleDAO implements iRole {
+    @Override
+    public void save(Role role) {
+        EntityManager em = EntityManagerAdmin.getInstance();
+        em.getTransaction().begin();
+        em.persist(role);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public Role getRole(int id) {
+        EntityManager em = EntityManagerAdmin.getInstance();
+        return em.find(Role.class, id);
+    }
+
+    @Override
+    public void update(Role role) {
+        EntityManager em = EntityManagerAdmin.getInstance();
+        em.getTransaction().begin();
+        em.merge(role);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public void delete(Role role) {
+        EntityManager em = EntityManagerAdmin.getInstance();
+        em.getTransaction().begin();
+        em.remove(role);
+        em.getTransaction().commit();
+        em.close();
+    }
 }
--- src/main/java/interfaces/iRole.java
+++ src/main/java/interfaces/iRole.java
@@ -1,4 +1,10 @@
 package interfaces;
 
+import models.Role;
+
 public interface iRole {
+    public void save(Role role);
+    public Role getRole(int id);
+    public void update(Role role);
+    public void delete(Role role);
 }
