4fee287edd0cba166abcc629df30227449209dc5
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2024-10-08 15:46:11 -0600

    Main finalizado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
+import dao.RoleDAO;
+import dao.UserDAO;
+import models.Role;
+import models.User;
+
 public class Main {
     public static void main(String[] args) {
+        RoleDAO roleDAO = new RoleDAO();
+        Role admin = new Role();
+        admin.setRole_type("admin");
+        roleDAO.save(admin);
+        Role developer = new Role();
+        developer.setRole_type("developer");
+        roleDAO.save(developer);
+        Role read = new Role();
+        read.setRole_type("read only");
+        roleDAO.save(read);
 
+        UserDAO userDAO = new UserDAO();
+        User karen = new User();
+        karen.setUsername("littlesaturnalien");
+        karen.setPassword("2004");
+        karen.setFull_name("Karen Mariza Rosa Yolanda Fonseca Vega");
+        karen.setPhone_number("86956877");
+        karen.setEmail("kmfvega@gmail.com");
+        karen.setRole(roleDAO.getRole(1));
+        karen.setStatus(true);
+        userDAO.save(karen);
     }
 }
